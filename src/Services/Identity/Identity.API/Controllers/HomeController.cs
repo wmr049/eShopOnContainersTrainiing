c18f6c8ae7bdbebbc04ef4602d1d49e47b7dd88e
Milton Reis <wmr049@gmail.com>
2018-06-05 06:00:27 -0300

    Tratamento Erro Identity

---
+++ src/Services/Identity/Identity.API/Controllers/HomeController.cs
@@ -0,0 +1,36 @@
+﻿using Identity.API.Models;
+using IdentityServer4.Services;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Identity.API.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly IIdentityServerInteractionService _interaction;
+
+        public HomeController(IIdentityServerInteractionService interaction)
+        {
+            _interaction = interaction;
+        }
+
+        public async Task<IActionResult> Error(string errorId)
+        {
+            var vm = new ErrorViewModel();
+
+            // retrieve error details from identityserver
+            var message = await _interaction.GetErrorContextAsync(errorId);
+            if (message != null)
+            {
+                vm.Error = message;
+            }
+            
+            return BadRequest(vm);
+            
+        }
+
+    }
+}
